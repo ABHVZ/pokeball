75527432d0b063b80ee4bbfaa4d180d8e0f8399a
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-10-31 17:58:21 -0400

    fix(seed): add user array to seed file

--- seed/seed.js
+++ seed/seed.js
@@ -2,16 +2,38 @@ const Promise = require('bluebird');
 
 const db = require('../server/db');
 const Pokemon = require('../server/db/models').Pokemon
+const User = require('../server/db/models').User
 
 const data = require('./seed.json');
 
+const userData = [
+  {
+    email: 'veekas@veekasmeansprogress.com',
+    firstName: 'Veekas',
+    lastName: 'Shrivastava'
+  },
+  {
+    email: 'bryanle52@gmail.com',
+    firstName: 'Bryan',
+    lastName: 'Le'
+  }, {
+    email: 'hkai.liu@gmail.com',
+    firstName: 'Nick',
+    lastName: 'Liu'
+  }
+]
 
 db.sync({force: true})
 .then(function () {
   console.log("Dropped old pokemon, now inserting pokemon");
   return Promise.map(data, function (pokemon) {
-    console.log(pokemon)
+    // console.log(pokemon)
     return Pokemon.create(pokemon);
+  })
+})
+.then(function () {
+  return Promise.map(userData, function (user) {
+    return User.create(user);
   })
 })
 .then(function () {
