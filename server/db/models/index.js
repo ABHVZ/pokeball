42eff12698fd27749739ad9e7293c0327f3e439c
Nicholas Liu <hkai.liu@gmail.com>
2017-10-31 17:01:09 -0400

    fix(model): fix model association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,45 +1,13 @@
-const User = require('./user')
-const Address = require('./address')
-const Order = require('./order')
-const Sequelize = require('sequelize');
+const User = require('./user');
+const Address = require('./address');
+const Order = require('./order');
 const Pokemon = require('./pokemon.js');
-const db = require('../');
 
-//Order: address, user, pokemon(item)
-Order.hasOne(Address);
-// Order.hasMany(Pokemon, {as: 'item'});
+Order.belongsTo(Address);
 Order.belongsTo(User);
-
-//User: address
-User.hasOne(Address); //possibly many in later edition
-
-//Address:
-//no associations needed
-
-//Types:
-//no associations needed
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
-
-Order.belongsToMany(Address);
-Address.hasMany(Order);
-Address.belongsTo(User);
-Order.belongsTo(User);
-User.hasMany(Order);
+User.hasOne(Address);
 
 module.exports = {
-    db: db,
     Order: Order,
     Address: Address,
     User: User,
