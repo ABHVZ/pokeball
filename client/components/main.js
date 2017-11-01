0d2bf362efe7703dc4ce31e0cbe9e84c15af7781
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-11-01 14:37:14 -0400

    feat(navbar): update navbar component and UI

--- client/components/main.js
+++ client/components/main.js
@@ -17,17 +17,17 @@ const Main = (props) => {
       <h1>PokeBall</h1>
       <nav>
         {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
+        //   isLoggedIn
+        //     ? <div>
+        //       {/* The navbar will show these links after you log in */}
+        //       <Link to="/home">Home</Link>
+        //       <a href="#" onClick={handleClick}>Logout</a>
+        //     </div>
+        //     : <div>
+        //       {/* The navbar will show these links before you log in */}
+        //       <Link to="/login">Login</Link>
+        //       <Link to="/signup">Sign Up</Link>
+        //     </div>
         }
       </nav>
       <hr />
