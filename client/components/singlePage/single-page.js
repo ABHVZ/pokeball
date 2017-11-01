544e24c7bd6ce11d02defd3841af612491a57177
Bryan Le <bryan@bryanle.io>
2017-11-01 12:47:55 -0400

    feat: single page created

---
+++ client/components/singlePage/single-page.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SinglePage extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div>Test single page</div>
+		)
+	}
+}
+
+export default SinglePage;
