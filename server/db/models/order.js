3f22880124c1abfa2c2e5bf0bd2a144b6cc01c6a
Veekas Shrivastava <veekas@users.noreply.github.com>
2017-11-02 11:35:38 -0400

    removed empty line

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-
 const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM('PROCESSING', 'SHIPPED', 'DELIVERED'),
