7f27373fe2eff25aa829919a89a2cfd9d210a1a8
Nicholas Liu <hkai.liu@gmail.com>
2017-11-03 11:42:08 -0400

    cart boilerplate created

--- client/components/cartPage/cartPage.js
+++ client/components/cartPage/cartPage.js
@@ -1,1 +1,32 @@
-//this is cart page
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import * as actions from '../../store'
+
+//only import what we need - below is carry-over from single-page
+import { Container, Grid, Image, Input, Button, Table, Dropdown } from 'semantic-ui-react';
+
+class cartPage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+        //empty state
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+  	console.log('cartPage rendered')
+    return (
+        <h1>Cart Page is displaying</h1>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+	return {
+	}
+}
+export default connect(mapStateToProps, actions)(cartPage);
