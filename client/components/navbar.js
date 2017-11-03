6802cf18607123f47c2c3240da3639af57caf016
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-11-03 15:55:40 -0400

    style(navbar): clean up some loose comments

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Menu, Button, Image, Icon, Container } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { Link, NavLink, withRouter } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import Search from './search';
 
 class Navbar extends Component {
--- client/components/search.js
+++ client/components/search.js
@@ -5,50 +5,11 @@ import { withRouter } from 'react-router-dom';
 import _ from 'lodash';
 import axios from 'axios';
 
-// const source = [
-//   {
-//     "title": "Blick - Streich",
-//     "description": "Optional multi-tasking neural-net",
-//     "image": "https://s3.amazonaws.com/uifaces/faces/twitter/anjhero/128.jpg",
-//     "price": "$50.20"
-//   },
-//   {
-//     "title": "Rogahn Inc",
-//     "description": "Ergonomic optimizing moderator",
-//     "image": "https://s3.amazonaws.com/uifaces/faces/twitter/ryuchi311/128.jpg",
-//     "price": "$60.00"
-//   },
-//   {
-//     "title": "Corwin, Zemlak and O'Reilly",
-//     "description": "User-friendly 3rd generation orchestration",
-//     "image": "https://s3.amazonaws.com/uifaces/faces/twitter/robinclediere/128.jpg",
-//     "price": "$27.11"
-//   },
-//   {
-//     "title": "Stiedemann - Murray",
-//     "description": "Multi-lateral multimedia instruction set",
-//     "image": "https://s3.amazonaws.com/uifaces/faces/twitter/buddhasource/128.jpg",
-//     "price": "$41.54"
-//   },
-//   {
-//     "title": "MacGyver - Harris",
-//     "description": "Multi-layered directional task-force",
-//     "image": "https://s3.amazonaws.com/uifaces/faces/twitter/markretzloff/128.jpg",
-//     "price": "$65.74"
-//   }
-// ]
-
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-      //   isLoading: false,
-      //   results: [],
-      //   value: ''
-      // }
       this.handleSearchChange = this.handleSearchChange.bind(this);
       this.handleResultSelect = this.handleResultSelect.bind(this);
-      // this.onSubmit = this.onSubmit.bind(this);
     }
 
     componentWillMount() {
@@ -67,22 +28,18 @@ class SearchBar extends Component {
       this.setState({ isLoading: true, value })
       setTimeout(() => {
         if (this.state.value.length < 1) return this.resetComponent();
-        const re = new RegExp(_.escapeRegExp(this.state.value)) // , 'i'
-        axios.get(`/api/pokemon/search${re}`) // json
-          // .then(pokemon => pokemon.json())
+        const re = new RegExp(_.escapeRegExp(this.state.value))
+        axios.get(`/api/pokemon/search${re}`)
           .then(pokemon => {
             this.setState({
               isLoading: false,
               results: pokemon.data
-              // results: _.filter(source, isMatch),
             })
           })
-        // const isMatch = result => re.test(result.title)
     }, 500)
   }
 
   render() {
-    // const pokemon = this.props.allPokemon;
     const { isLoading, value, results } = this.state
 
     return (
@@ -99,13 +56,6 @@ class SearchBar extends Component {
             {...this.props}
           />
       </Container>
-        {
-          // <Input
-          //   icon="search"
-          //   fluid
-          //   placeholder="Search..."
-          // />
-        }
       </div>
     )
   }
