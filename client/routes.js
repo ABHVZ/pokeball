4a4322657b7ae33e467dfbad4e1b87152e4b6d9a
Bryan Le <bryan@bryanle.io>
2017-11-03 11:27:33 -0400

    feat: single page functioning

--- client/routes.js
+++ client/routes.js
@@ -4,8 +4,7 @@ import { Router } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import { Main, Login, Signup, UserHome, HomePage, Navbar } from './components';
-import SinglePage from './components/singlePage/single-page';
+import { Main, Login, Signup, UserHome, HomePage, Navbar, SinglePage } from './components';
 import { me, fetchAllPokemon } from './store'
 
 /**
