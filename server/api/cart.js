d0fc624a5a64a40b04a125ac8c0c50cf49b66341
Nicholas Liu <hkai.liu@gmail.com>
2017-11-05 09:44:36 -0500

    fix: sessions.cart should include quantity

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,8 +4,16 @@ module.exports = router
 router.post('/', (req, res, next) => {
     // console.log('this is body: ', req.body)
     // console.log('this is previous session', req.session.cart)
+    let pokemonObj = req.body.singlePokemon
     if (!req.session.cart) {req.session.cart = []}
-    else { req.session.cart.push(req.body) }
+    else {
+        let existingPokemons = [];
+        req.session.cart.forEach(array => existingPokemons.push(array[0].id));
+        if (existingPokemons.indexOf(pokemonObj.id) !== -1) {
+            req.session.cart
+        }
+        req.session.cart.push([pokemonObj, qty]);
+    }
     console.log('this is now session', req.session.cart);
     res.sendStatus(200)
 })
