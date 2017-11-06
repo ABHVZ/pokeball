6bcac7d6a13469542da22d6339cf0e2ddf44df44
Aaron W <aaronw2291@gmail.com>
2017-11-06 11:46:37 -0500

    feat(user): created frontend user page

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,18 +1,345 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { Button, Checkbox, Container, Form, Menu } from 'semantic-ui-react';
+
+
+const states = [
+  {
+    key: 'AL',
+    value: 'AL',
+    text: 'Alabama',
+  },
+  {
+    key: 'AK',
+    value: 'AK',
+    text: 'Alaska',
+  },
+  {
+    key: 'AZ',
+    value: 'AZ',
+    text: 'Arizona',
+  },
+  {
+    key: 'AR',
+    value: 'AR',
+    text: 'Arkansas',
+  },
+  {
+    key: 'CA',
+    value: 'CA',
+    text: 'California',
+  },
+  {
+    key: 'CO',
+    value: 'CO',
+    text: 'Colorado',
+  },
+  {
+    key: 'CT',
+    value: 'CT',
+    text: 'Connecticut',
+  },
+  {
+    key: 'DE',
+    value: 'DE',
+    text: 'Delaware',
+  },
+  {
+    key: 'FL',
+    value: 'FL',
+    text: 'Florida',
+  },
+  {
+    key: 'GA',
+    value: 'GA',
+    text: 'Georgia',
+  },
+  {
+    key: 'HI',
+    value: 'HI',
+    text: 'Hawaii',
+  },
+  {
+    key: 'ID',
+    value: 'ID',
+    text: 'Idaho',
+  },
+  {
+    key: 'IL',
+    value: 'IL',
+    text: 'Illinois',
+  },
+  {
+    key: 'IN',
+    value: 'IN',
+    text: 'Indiana',
+  },
+  {
+    key: 'IA',
+    value: 'IA',
+    text: 'Iowa',
+  },
+  {
+    key: 'KS',
+    value: 'KS',
+    text: 'Kansas',
+  },
+  {
+    key: 'KY',
+    value: 'KY',
+    text: 'Kentucky',
+  },
+  {
+    key: 'LA',
+    value: 'LA',
+    text: 'Louisiana',
+  },
+  {
+    key: 'ME',
+    value: 'ME',
+    text: 'Maine',
+  },
+  {
+    key: 'MD',
+    value: 'MD',
+    text: 'Maryland',
+  },
+  {
+    key: 'MA',
+    value: 'MA',
+    text: 'Massachusetts',
+  },
+  {
+    key: 'MI',
+    value: 'MI',
+    text: 'Michigan',
+  },
+  {
+    key: 'MN',
+    value: 'MN',
+    text: 'Minnesota',
+  },
+  {
+    key: 'MS',
+    value: 'MS',
+    text: 'Mississippi',
+  },
+  {
+    key: 'MO',
+    value: 'MO',
+    text: 'Missouri',
+  },
+  {
+    key: 'MT',
+    value: 'MT',
+    text: 'Montana',
+  },
+  {
+    key: 'NE',
+    value: 'NE',
+    text: 'Nebraska',
+  },
+  {
+    key: 'NV',
+    value: 'NV',
+    text: 'Nevada',
+  },
+  {
+    key: 'NH',
+    value: 'NH',
+    text: 'New Hampshire',
+  },
+  {
+    key: 'NJ',
+    value: 'NJ',
+    text: 'New Jersey',
+  },
+  {
+    key: 'NM',
+    value: 'NM',
+    text: 'New Mexico',
+  },
+  {
+    key: 'NY',
+    value: 'NY',
+    text: 'New York',
+  },
+  {
+    key: 'NC',
+    value: 'NC',
+    text: 'North Carolina',
+  },
+  {
+    key: 'ND',
+    value: 'ND',
+    text: 'North Dakota',
+  },
+  {
+    key: 'OH',
+    value: 'OH',
+    text: 'Ohio',
+  },
+  {
+    key: 'OK',
+    value: 'OK',
+    text: 'Oklahoma',
+  },
+  {
+    key: 'OR',
+    value: 'OR',
+    text: 'Oregon',
+  },
+  {
+    key: 'PA',
+    value: 'PA',
+    text: 'Pennsylvania',
+  },
+  {
+    key: 'PR',
+    value: 'PR',
+    text: 'Puerto Rico',
+  },
+  {
+    key: 'RI',
+    value: 'RI',
+    text: 'Rhode Island',
+  },
+  {
+    key: 'SC',
+    value: 'SC',
+    text: 'South Carolina',
+  },
+  {
+    key: 'SD',
+    value: 'SD',
+    text: 'South Dakota',
+  },
+  {
+    key: 'TN',
+    value: 'TN',
+    text: 'Tennessee',
+  },
+  {
+    key: 'TX',
+    value: 'TX',
+    text: 'Texas',
+  },
+  {
+    key: 'UT',
+    value: 'UT',
+    text: 'Utah',
+  },
+  {
+    key: 'VT',
+    value: 'VT',
+    text: 'Vermont',
+  },
+  {
+    key: 'VI',
+    value: 'VI',
+    text: 'Virgin Islands',
+  },
+  {
+    key: 'VA',
+    value: 'VA',
+    text: 'Virginia',
+  },
+  {
+    key: 'WA',
+    value: 'WA',
+    text: 'Washington',
+  },
+  {
+    key: 'WV',
+    value: 'WV',
+    text: 'West Virginia',
+  },
+  {
+    key: 'WI',
+    value: 'WI',
+    text: 'Wisconsin',
+  },
+  {
+    key: 'WY',
+    value: 'WY',
+    text: 'Wyoming',
+  }
+]
+
 
 /**
  * COMPONENT
  */
-export const UserHome = (props) => {
-  const {email} = props
+export class UserHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 'account',
+      disabled: true
+    }
+  }
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
+  handleMenuClick = (e, { name }) => this.setState({ activeItem: name })
+  handleEditButton = () => this.setState({ disabled: false })
+  handleSubmitButton = () => { this.setState({ disabled: true }) }
+
+
+  render() {
+    const { email, firstName, lastName } = this.props.user
+    const { activeItem, disabled } = this.state
+
+    return (
+      <div className="home-page-container">
+        <div className="sidebar">
+          <Menu vertical>
+            <Menu.Item name="account" active={activeItem === 'account'} href="/home" onClick={this.handleMenuClick}>Account</Menu.Item>
+            <Menu.Item name="orders" active={activeItem === 'orders'} href="/user/orders" onClick={this.handleMenuClick}>Orders</Menu.Item>
+            <Menu.Item name="settings" active={activeItem === 'settings'} href="/user/settings" onClick={this.handleMenuClick}>Settings</Menu.Item>
+          </Menu>
+        </div>
+
+        <Container>
+          <h3>Welcome, {email}</h3>
+          <h4>General Information</h4>
+          <Container>
+            <Form>
+              <Form.Input readOnly={disabled} label="First Name" value={firstName} />
+              <Form.Input readOnly={disabled} label="Last Name" value={lastName} />
+              <Form.Input required readOnly={disabled} label="Email" value={email} />
+            </Form>
+
+
+            <h4>Shipping Address</h4>
+            <Form>
+              <Form.Input required readOnly={disabled} label="Street Address" />
+              <Form.Input readOnly={disabled} placeholder="Apartment, suite, etc." />
+              <Form.Input required readOnly={disabled} label="City" />
+              <Form.Dropdown required disabled={disabled} search selection options={states} label="State" />
+              <Form.Input required readOnly={disabled} label="Zip Code" />
+            </Form>
+
+            <h4>
+              Billing Address <Checkbox label="Same as Shipping" />
+            </h4>
+            <Form>
+              <Form.Input required readOnly={disabled} label="Street Address" />
+              <Form.Input readOnly={disabled} placeholder="Apartment, suite, etc." />
+              <Form.Input required readOnly={disabled} label="City" />
+              <Form.Dropdown required disabled={disabled} search selection options={states} label="State" />
+              <Form.Input required readOnly={disabled} label="Zip Code" />
+            </Form>
+          </Container>
+
+          <Container>
+            <Button color="orange" onClick={this.handleEditButton}>Edit</Button>
+            <Button color="green" onClick={this.handleSubmitButton}>Submit</Button>
+          </Container>
+        </Container>
+
+      </div >
+    )
+  }
 }
 
 /**
@@ -20,7 +347,7 @@ export const UserHome = (props) => {
  */
 const mapState = (state) => {
   return {
-    email: state.user.email
+    user: state.user
   }
 }
 
